622bbfc9662f74c6e41b0727c01290b947e37060
Rajdeep <rajdeep.hascode@gmail.com>
2023-06-26 18:25:26 +0530

    shortener, url lists completion

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,5 +1,55 @@
 export default function List() {
-    return <ul className="list">
-        lists
+    return <ul className="list grid grid-cols-1 gap-5">
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        <li className="list-item">
+            <div className="links">
+                <a href="" className="full-url">https://url-shortener-microservice.freecodecamp.rocks/</a>
+                <a href="" className="short-url font-medium">https://shrtco.de/cYZjV</a>
+            </div>
+            <button type="button" className="btn-cta btn-copy">Copy</button>
+        </li>
+        
+       
     </ul>
 }
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -7,6 +7,9 @@ export default function Main() {
     return <main>
         <Shortener />
         <List />
+        <div className="flex justify-center">
+            <TiDeleteOutline className="btn-cross" /> 
+        </div>
         <section className="stats pb-11 lg:pb-12">
             <h3 className="title pb-2">Advanced Statistics</h3>
             <p className="subtitle">Track how many clicks your shortened URLs receive and measure their performance.</p>
