b70fca357a472fa798cd58b48911225f18b8d86b
Rajdeep <rajdeep.hascode@gmail.com>
2023-06-26 13:05:56 +0530

    shortener, boost area completion

--- client/src/components/Shortener.jsx
+++ client/src/components/Shortener.jsx
@@ -2,10 +2,10 @@ export default function Shortener() {
     return <div className="shortener rounded-lg">
         <form className="">
             <div className="input-area">
-                <input type="url" placeholder="Shorten a link here..." />
+                <input type="url" placeholder="Shorten a link here..." id="input" />
                 <p className="warning">Please add a link</p>
             </div>
-            <button className="btn-cta rounded-lg" type="button">Shorten it!</button>
+            <button className="btn-cta" type="button">Shorten it!</button>
         </form>
     </div>
 }
